59c60f0118650cc77075da0c6f5631894a9e14ce
christophergandrud <christopher.gandrud@gmail.com>
2017-05-30 11:00:37 -0400

    function documentation

--- R/beta_plot.R
+++ R/beta_plot.R
@@ -1,3 +1,24 @@
+#' Draw values from a beta distribution and plot the probability density
+#' function
+#'
+#' @param n number of observations to draw
+#' @param a non-negative alpha parameter of the beta distribution
+#' @param b non-negative beta parameter of the beta distribution
+#'
+#' @details The Beta distribution with parameters \eqn{a} and \eqn{b} has
+#' density:
+#'
+#' \deqn{
+#'     \Gamma(a+b)/(\Gamma(a)\Gamma(b))x^(a-1)(1-x)^(b-1)
+#' }
+#'
+#' for \eqn{a > 0}, \eqn{b > 0} and \eqn{0 \le x \le 1}.
+#'
+#' @examples
+#' # Draw from beta distribution with parameters a = 1 and b = 3
+#' beta_plot(a = 1, b = 3)
+#'
+#' @seealso \code{\link{rbeta}}, \code{\link{geom_density}}
 #' @import ggplot2
 #' @export
 
