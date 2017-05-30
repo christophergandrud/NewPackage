c520bf0b283dd4b18a9d25c43b088cd9c21e3859
christophergandrud <christopher.gandrud@gmail.com>
2017-05-30 07:29:34 -0400

    beta_plot added

---
+++ R/beta_plot.R
@@ -0,0 +1,15 @@
+
+
+beta_plot <- function(n = 10000, a = 1, b = 3) {
+    # draw distributions
+    sims <- rbeta(n = n, shape1 = a, shape2 = b)
+
+    # convert to data frame for ggplot2 compatability
+    sims <- data.frame(x = sims)
+
+    # plot probability density function
+    ggplot(sims, aes(x)) +
+        geom_density() +
+        xlab("") + ylab("Probability Density Function") +
+        theme_bw()
+}
